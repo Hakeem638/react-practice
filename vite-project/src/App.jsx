08af0db6629115b43034977c9e473b6b7af0c765
Hakeem638 <harunahakeem638@gmail.com>
2026-01-01 01:17:44 -1200

    created the movies object and used .map to render each movie

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 import MovieCard from './components/MovieCard'
 
 function App() {
+  const movies=[
+    { title: 'The Woman King', release_date: '4 Nov, 2024' },
+    { title: 'Avatar 3', release_date: '20 Dec, 2024' },
+    { title: 'Guardians of the Galaxy Vol. 3', release_date: '5 May, 2024' },
+    { title: 'Dune: Part Two', release_date: '17 Oct, 2024' },
+    { title: 'Mission: Impossible – Dead Reckoning Part One', release_date: '12 Jul, 2024' },
+  ];
 
   return (
     <>
-      <MovieCard movie={{ title: 'The Woman King', release_date: '4 Nov, 2024' }} />
+      {movies.map((movie, index) => (
+        <MovieCard key={index} movie={movie} />
+      ))}
     </>
   )
 }
