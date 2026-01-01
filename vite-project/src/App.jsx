8cb27d093c3ded3a3337afff9dee7521b68f5412
Hakeem638 <harunahakeem638@gmail.com>
2026-01-01 03:02:45 -1200

    had a render issue I'm trying to resolve

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,19 @@
+import {Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
+import Favorite from "./pages/Favorite"
+import NavBar from "./components/NavBar"
 
 function App() {
- 
   return (
-    <>
-    <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
---
+++ vite-project/src/pages/Favorite.jsx
@@ -0,0 +1,11 @@
+
+function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <h2>No favorite movies yet</h2>
+      <p>You haven't added any movies to your favorites. Start exploring and add some!</p>
+    </div>
+  )
+}
+
+export default Favorite
