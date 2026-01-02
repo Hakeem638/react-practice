ca1f66ceca176d1e60e266bd9e323817d279f5b3
Hakeem638 <harunahakeem638@gmail.com>
2026-01-02 10:39:42 -1200

    solved the render issue, I did not save when I imported BrowserRouter in the main.jsx file

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,20 @@
 import {Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
 import Favorite from "./pages/Favorite"
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar"  
 
 function App() {
+ 
   return (
-    <div>
+    <>
       <NavBar />
-      <main className="main-content">
+      <main className="main-container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Favorite />} />
         </Routes>
-      </main>
-    </div>
+      </main> 
+    </>          
   )
 }
 
