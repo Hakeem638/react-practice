35c808e2b6f3e5378d96900bfef3853aaeba2b93
Hakeem638 <harunahakeem638@gmail.com>
2026-01-02 22:57:30 -1200

    added all the css files and did the importation to each file

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,8 @@
 import {Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
 import Favorite from "./pages/Favorite"
-import NavBar from "./components/NavBar"  
+import NavBar from "./components/NavBar"
+import "./css/App.css"  
 
 function App() {
  
